97bd11a7e7fa6c2e5bed8b2aa9de62908a184816
yongzhuo <31341349+yongzhuo@users.noreply.github.com>
2019-05-30 22:00:46 +0800

    Update extract_keras_bert_feature.py

--- FeatureProject/bert/extract_keras_bert_feature.py
+++ FeatureProject/bert/extract_keras_bert_feature.py
@@ -2,7 +2,7 @@
 # !/usr/bin/python
 # @time     :2019/5/8 20:04
 # @author   :Mo
-# @function :
+# @function :extract feature of bert and keras
 
 from conf.feature_config import gpu_memory_fraction, config_name, ckpt_name, vocab_file, max_seq_len, layer_indexes
 from keras_bert import load_trained_model_from_checkpoint, Tokenizer
@@ -19,6 +19,7 @@ import os
 graph = None
 model = None
 
+
 # gpu配置与使用率设置
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 config = tf.ConfigProto()
@@ -34,19 +35,47 @@ class KerasBertVector():
         graph = tf.get_default_graph()
         global model
         model = load_trained_model_from_checkpoint(self.config_path, self.checkpoint_path,
-                                                        seq_len=self.max_seq_len)
-        model.summary(120)
-        # 如果只选一层，就只取对应那一层的weight
-        if len(layer_indexes) == 1:
-            encoder_layer = model.get_layer(index=len(model.layers)-2).output
+                                                   seq_len=self.max_seq_len)
+        print(model.output)
+        print(len(model.layers))
+        # lay = model.layers
+        #一共104个layer，其中前八层包括token,pos,embed等，
+        # 每4层（MultiHeadAttention,Dropout,Add,LayerNormalization）
+        # 一共24层
+        layer_dict = []
+        layer_0 = 7
+        for i in range(24):
+            layer_0 = layer_0 + 4
+            layer_dict.append(layer_0)
+        # 输出它本身
+        if len(layer_indexes) == 0:
+            encoder_layer = model.output
+        # 分类如果只有一层，就只取最后那一层的weight，取得不正确
+        elif len(layer_indexes) == 1:
+            if layer_indexes[0] in [i+1 for i in range(23)]:
+                encoder_layer = model.get_layer(index=layer_dict[layer_indexes[0]]).output
+            else:
+                encoder_layer = model.get_layer(index=layer_dict[-1]).output
         # 否则遍历需要取的层，把所有层的weight取出来并拼接起来shape:768*层数
         else:
-            # layer_indexes must be [1,2,3,......12]
-            all_layers = [model.get_layer(index=lay).output for lay in layer_indexes]
-            encoder_layer = k_keras.concatenate(all_layers, -1)
+            # layer_indexes must be [1,2,3,......12...24]
+            # all_layers = [model.get_layer(index=lay).output if lay is not 1 else model.get_layer(index=lay).output[0] for lay in layer_indexes]
+            all_layers = [model.get_layer(index=layer_dict[lay-1]).output if lay in [i+1 for i in range(23)]
+                          else model.get_layer(index=layer_dict[-1]).output  #如果给出不正确，就默认输出最后一层
+                          for lay in layer_indexes]
+            print(layer_indexes)
+            print(all_layers)
+            # 其中layer==1的output是格式不对，第二层输入input是list
+            all_layers_select = []
+            for all_layers_one in all_layers:
+                all_layers_select.append(all_layers_one)
+            encoder_layer = Add()(all_layers_select)
+            print(encoder_layer.shape)
+        print("KerasBertEmbedding:")
+        print(encoder_layer.shape)
         output_layer = NonMaskingLayer()(encoder_layer)
         model = Model(model.inputs, output_layer)
-
+        # model.summary(120)
         # reader tokenizer
         self.token_dict = {}
         with codecs.open(self.dict_path, 'r', 'utf8') as reader:
@@ -55,6 +84,7 @@ class KerasBertVector():
                 self.token_dict[token] = len(self.token_dict)
 
         self.tokenizer = Tokenizer(self.token_dict)
+
 
     def bert_encode(self, texts):
         # 文本预处理
